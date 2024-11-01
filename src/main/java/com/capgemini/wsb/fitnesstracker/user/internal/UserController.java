a83bc9bebd89c0e4d6b8c8bb2eb6aaa52979e103
lzlocki <lzlocki@poczta.fm>
2024-11-01 14:11:33 +0100

    documents controller layer

--- src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserController.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserController.java
@@ -23,6 +23,10 @@ class UserController {
 
     private final UserMapper userMapper;
 
+    /**
+     * Retrive all user
+     * @return list of user objects
+     */
     @GetMapping("")
     public List<UserDto> getAllUsers() {
         return userService.findAllUsers()
@@ -31,6 +35,10 @@ class UserController {
                           .toList();
     }
 
+    /**
+     * Retrive all users basic data like first name, last name
+     * @return list of UserBasicDto objects
+     */
     @GetMapping("/simple")
     public List<UserBasicDto> getAllUsersBasicData() {
         return userService.findAllUsers()
@@ -39,6 +47,11 @@ class UserController {
                 .toList();
     }
 
+    /**
+     * Retrive user by given ID
+     * @param id the primary key of user to be updated
+     * @return userDto object
+     */
     @GetMapping("/{id}")
     public UserDto getUserById(@PathVariable Long id) {
         Optional<User> optionalUser = userService.getUserById(id);
@@ -51,6 +64,11 @@ class UserController {
 
     }
 
+    /**
+     * Retrive user by given email
+     * @param email user email
+     * @return list of UserBasicEmailDto object
+     */
     @GetMapping("/email")
     public List<UserBasicEmailDto> findUserByEmail(@RequestParam String email) {
         System.out.println("Received email: " + email);
@@ -64,24 +82,47 @@ class UserController {
         return userList;
     }
 
+    /**
+     * Retrive all users which are older then birthday date
+     * @param birthDate user birth date
+     * @return list of users older then given birthDAte parameter
+     */
     @GetMapping("/older/{birthDate}")
     public List<User> getOlderUsers(@PathVariable String birthDate){
         LocalDate date = LocalDate.parse(birthDate);
         return userService.getUserOlderThenDate(date);
     }
 
+    /**
+     * Add user
+     * @param userDto user information
+     * @return status Ok and user object
+     * @throws InterruptedException if operation is interrupted
+     */
     @PostMapping("")
     public ResponseEntity<User> addUser(@RequestBody UserDto userDto) throws InterruptedException {
         User userCreated = userService.createUser(userMapper.toEntity(userDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
+    /**
+     * Update user with specific ID
+     *
+     * @param userId the primary key of user to be updated
+     * @param updatedUserDto updated user information
+     * @return updated user object
+     */
     @PutMapping("/{userId}")
     public User updateUser(@PathVariable Long userId, @RequestBody UserDto updatedUserDto) {
         User userToUpdate = userMapper.toEntity(updatedUserDto);
         return userService.updateUser(userId, userToUpdate);
     }
 
+    /**
+     * Delete user
+     * @param userId the primary key of user to be deleted
+     * @return ResponseEntity with HTTP status and message
+     */
     @DeleteMapping("/{userId}")
     public ResponseEntity<?> deleteUser(@PathVariable Long userId) {
         try {
