4509277705e1c49e29b5723f69bec0f77c42d6e3
lzlocki <lzlocki@poczta.fm>
2024-11-09 11:57:07 +0100

    add training dto

---
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/api/TrainingDto.java
@@ -0,0 +1,41 @@
+package com.capgemini.wsb.fitnesstracker.training.api;
+
+import com.capgemini.wsb.fitnesstracker.training.internal.ActivityType;
+import com.capgemini.wsb.fitnesstracker.user.api.User;
+import jakarta.annotation.Nullable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class TrainingDto {
+    @Nullable
+    private Long id;
+    private User user;
+    private Date startTime;
+    private Date endTime;
+    private ActivityType activityType;
+    private double distance;
+    private double averageSpeed;
+
+    public TrainingDto(Long id,
+                       User user,
+                       Date startTime,
+                       Date endTime,
+                       ActivityType activityType,
+                       double distance,
+                       double averageSpeed) {
+        this.user = user;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.activityType = activityType;
+        this.distance = distance;
+        this.averageSpeed = averageSpeed;
+    }
+
+}
