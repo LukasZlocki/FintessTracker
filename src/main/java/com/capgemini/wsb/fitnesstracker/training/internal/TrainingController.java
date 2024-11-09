49a720ff872106ad69ffb1dcbd4c328212e1a7a9
lzlocki <lzlocki@poczta.fm>
2024-11-09 12:08:37 +0100

    update to get all trainings

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingController.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingController.java
@@ -1,9 +1,12 @@
 package com.capgemini.wsb.fitnesstracker.training.internal;
 
+import com.capgemini.wsb.fitnesstracker.training.api.TrainingDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/trainings")
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TrainingController {
 
     private final TrainingServiceImpl trainingService;
+    private final TrainingMapper trainingMapper;
 
     @GetMapping("")
     public List<TrainingDto> GetAllTrainings() {
