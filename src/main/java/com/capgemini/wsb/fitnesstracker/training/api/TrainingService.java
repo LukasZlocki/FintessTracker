98636d158498586972fb135e3dfd31a37e2b4f0b
lzlocki <lzlocki@poczta.fm>
2024-11-24 14:57:39 +0100

    add training create method to interface

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/api/TrainingService.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/api/TrainingService.java
@@ -1,8 +1,24 @@
 package com.capgemini.wsb.fitnesstracker.training.api;
 
+import com.capgemini.wsb.fitnesstracker.user.api.UserNotFoundException;
+
 import java.util.List;
 
 public interface TrainingService {
+
+    /**
+     * Get all trainings
+     *
+     * @return List of trainings object class
+     */
     public List<Training> getAllTrainings();
 
+    /**
+     * Create new training
+     * Throws a {@link UserNotFoundException} when trying to create training for non-existent user
+     *
+     * @param trainingInputDto Dto with user id data to be mapped onto training entity and saved to db
+     * @return {@link TrainingDto} of created training
+     */
+    TrainingDto createTraining(TrainingInputDto trainingInputDto) throws UserNotFoundException;
 }
