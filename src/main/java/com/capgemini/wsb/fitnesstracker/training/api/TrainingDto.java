ebaf07cb3e3ce5a6ba0e596e86dc8af7ef3bf494
lzlocki <lzlocki@poczta.fm>
2024-11-24 16:13:14 +0100

    add update model

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/api/TrainingDto.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/api/TrainingDto.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
 public class TrainingDto {
+    private Long id;
     private User user;
     private Date startTime;
     private Date endTime;
@@ -28,12 +29,25 @@ public class TrainingDto {
                        ActivityType activityType,
                        double distance,
                        double averageSpeed) {
+        this.id = id;
         this.user = user;
         this.startTime = startTime;
         this.endTime = endTime;
         this.activityType = activityType;
         this.distance = distance;
         this.averageSpeed = averageSpeed;
+    }
+
+    public TrainingDto updateTraining(TrainingDto trainingDto) {
+        return new TrainingDto(
+                trainingDto.id == null ? id : trainingDto.id,
+                trainingDto.getUser() == null ? user : trainingDto.getUser(),
+                trainingDto.getStartTime() == null ? startTime : trainingDto.getStartTime(),
+                trainingDto.getEndTime() == null ? endTime : trainingDto.getEndTime(),
+                trainingDto.getActivityType() == null ? activityType : trainingDto.getActivityType(),
+                trainingDto.getDistance() == 0 ? distance : trainingDto.getDistance(),
+                trainingDto.getAverageSpeed() == 0 ? averageSpeed : trainingDto.getAverageSpeed()
+        );
     }
 
 }
