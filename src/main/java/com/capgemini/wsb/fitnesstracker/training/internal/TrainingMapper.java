7560934784b2d5fee697a4f231536cf32e4c7d76
lzlocki <lzlocki@poczta.fm>
2024-11-24 15:59:33 +0100

    add aditional mappers for training creation purpose

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingMapper.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/internal/TrainingMapper.java
@@ -2,12 +2,15 @@ package com.capgemini.wsb.fitnesstracker.training.internal;
 
 import com.capgemini.wsb.fitnesstracker.training.api.Training;
 import com.capgemini.wsb.fitnesstracker.training.api.TrainingDto;
+import com.capgemini.wsb.fitnesstracker.training.api.TrainingInputDto;
+import com.capgemini.wsb.fitnesstracker.user.api.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TrainingMapper {
     TrainingDto toTrainingDto (Training training) {
         return new TrainingDto(
+                training.getId(),
                 training.getUser(),
                 training.getStartTime(),
                 training.getEndTime(),
@@ -16,5 +19,25 @@ public class TrainingMapper {
                 training.getAverageSpeed()
         );
 
+    }
+
+    Training toEntity(TrainingDto trainingDto) {
+        return new Training(
+                trainingDto.getUser(),
+                trainingDto.getStartTime(),
+                trainingDto.getEndTime(),
+                trainingDto.getActivityType(),
+                trainingDto.getDistance(),
+                trainingDto.getAverageSpeed());
+    }
+
+    TrainingDto inputDtoToTrainingDto(User user, TrainingInputDto trainingInputDto) {
+        return new TrainingDto(null,
+                user,
+                trainingInputDto.getStartTime(),
+                trainingInputDto.getEndTime(),
+                trainingInputDto.getActivityType(),
+                trainingInputDto.getDistance(),
+                trainingInputDto.getAverageSpeed());
     }
 }
