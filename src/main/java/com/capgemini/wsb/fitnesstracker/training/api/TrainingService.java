73f3e9b3c16015731b8d4d67b0bb64e397f6eecc
lzlocki <lzlocki@poczta.fm>
2024-11-24 16:07:53 +0100

    add training update method to interface

--- src/main/java/com/capgemini/wsb/fitnesstracker/training/api/TrainingService.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/training/api/TrainingService.java
@@ -21,4 +21,15 @@ public interface TrainingService {
      * @return {@link TrainingDto} of created training
      */
     TrainingDto createTraining(TrainingInputDto trainingInputDto) throws UserNotFoundException;
+
+    /**
+     * Updates training of specified {@param id} with the data in {@param trainingInputDto}
+     * Throws a {@link UserNotFoundException} when trying to update training for non-existent user
+     * Throws a {@link TrainingNotFoundException} when trying to update non-existent training
+     *
+     * @param id id of training to be updated
+     * @param trainingInputDto data used to replace current training with
+     * @return {@link TrainingDto} of updated training
+     */
+    TrainingDto updateTraining(Long id, TrainingInputDto trainingInputDto) throws UserNotFoundException, TrainingNotFoundException;
 }
